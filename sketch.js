bb83f5d453e1bbc95793f8f4e96c6e5473ef0291
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-10-20 12:05:39 -0500

    Fish+Background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let fishImg = [];
+let timer=0;
+let interval=750;
+let randomY
+let randomX
+
+function preload(){
+    let fish1=loadImage('BlueFish.png');
+    let fish2=loadImage('LightBlueFish.png');
+    let fish3=loadImage('TealFish.png');
+    let fish4=loadImage('TealFish2.png');
+    oceanBackground=loadImage('oceanBackground.jpg')
+    fishImg=[fish1,fish2,fish3,fish4];
+}
+
 function setup(){
+    createCanvas(windowWidth/1.2,windowHeight/1.2);
+    background(oceanBackground);
 
 }
 
 function draw(){
-    
+    randomY = random(height);
+    randomX = random(width);
+    let fishChoice=random(fishImg)
+
+    if(millis()-timer>interval) {
+        image(fishChoice,randomX,randomY,100,100);
+        timer=millis();
+    }
 }
+
+
